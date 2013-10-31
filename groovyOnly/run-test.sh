cec48c846160e54f10c8e8d93d4d6577c39bc5b4
dnoseda <damian.noseda@mercadolibre.com>
2013-10-31 00:07:00 -0300

    fixed

--- groovyOnly/run-test.sh
+++ groovyOnly/run-test.sh
@@ -2,4 +2,4 @@ if [ ! -f chromedriver ]; then
 
   wget "http://chromedriver.storage.googleapis.com/2.4/chromedriver_mac32.zip" -O temp.zip; unzip temp.zip; rm temp.zip
 fi
-grails -Dwebdriver.chrome.driver=$PWD/chromedriver test-app -unit Movements -echoOut -echoErr
+groovy -Dwebdriver.chrome.driver=$PWD/chromedriver MultiplierTest.groovy
