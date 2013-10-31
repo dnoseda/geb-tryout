a212399a14c0a543a96a931eca51ca92ca3683bf
dnoseda <damian.noseda@mercadolibre.com>
2013-10-30 23:47:44 -0300

    doesn't compile, going to remove java, add dependency and inherit

--- src/main/groovy/MultipleTest.groovy
+++ src/main/groovy/MultipleTest.groovy
@@ -4,13 +4,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
-
-@Grapes([
-	@Grab("org.gebish:geb-core:0.9.2"),
-	@Grab("org.seleniumhq.selenium:selenium-chrome-driver:2.26.0"),
-	@Grab("org.seleniumhq.selenium:selenium-support:2.26.0"),
-])
-
 import geb.Browser
 import com.saucelabs.junit.Parallelized;
 
